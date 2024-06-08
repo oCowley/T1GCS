5dea64b7ec3d7fccc3ff10ba753b801eb513b5d1
oCowley <142631209+oCowley@users.noreply.github.com>
2024-06-08 00:29:24 +0000

    alteração na main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
-gipublic class Main {
+public class Main {
     public static void main(String[] args) {
-        
+        Sistema sistema = new Sistema();
+        sistema.inicializarDados(); // Inicializa com dados de exemplo
+        sistema.executar(); // Inicia o sistema
     }
 }
+
